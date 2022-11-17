8e0c46feaaadaaaf9c303c10bc9583e5d2f83619
pushpakgupta <pushpak.gupta2@gmail.com>
2022-11-17 14:07:14 +0530

    connect shopify store

--- hydrogen.config.ts
+++ hydrogen.config.ts
@@ -4,8 +4,8 @@ export default defineConfig({
   shopify: {
     defaultCountryCode: 'US',
     defaultLanguageCode: 'EN',
-    storeDomain: 'hydrogen-preview.myshopify.com',
-    storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
+    storeDomain: 'hydrogenapp-test.myshopify.com/',
+    storefrontToken: '3cac9559297e323fc31255eff8a25389',
     storefrontApiVersion: '2022-07',
   },
   session: CookieSessionStorage('__session', {
